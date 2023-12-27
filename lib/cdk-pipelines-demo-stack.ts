6fba0c85786b4d644b22ee357b454842155cdcf6
Hossameldin Shahin <hshahin@vt.edu>
2023-12-27 11:23:29 -0500

    initial commit

--- lib/cdk-pipelines-demo-stack.ts
+++ lib/cdk-pipelines-demo-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+// aws-cdk-lib » aws_secretsmanager » Secret
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +10,12 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('hosamshahin/cdk-pipelines-demo', 'main'),
+        input: CodePipelineSource.gitHub('hosamshahin/cdk-pipelines-demo', 'main', {
+          authentication: cdk.SecretValue.secretsManager(
+            'lambda_container_cdk_pipeline_github', {
+            jsonField: 'github'
+          })
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
